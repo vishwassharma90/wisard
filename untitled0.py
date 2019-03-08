79b64b8ac5fc23344db077ecd9b968e3bdcb33e5
vishwassharma90 <37620104+vishwassharma90@users.noreply.github.com>
2019-03-08 16:31:25 +0100

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -1,14 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Feb 21 12:06:22 2019
+Created on Wed Mar  6 10:14:54 2019
 
-@author: vishwas
+@author: iss
 """
 
+from numba import njit,jit,vectorize
+from numba import cuda
 import numpy as np
+#import struct
+import random
+import time
 
-y_train = [0,1,2,3,4,5,6,7,8,9]
+@vectorize(['int32(int32)'], target = 'cuda')
+def addition(d):
+    for i in range(50000000):
+        i +=1
+    return d
+ 
+    
 
-x = y_train[2]
-print(x)
+
+if __name__ == "__main__":
+    
+    d0 = [1,2,3,6357,467,467,54]
+    
+#    d = [[[{'23':1 , '2':32},{'24':8 , '35':2}],[[647,5],[456,5],[35,4]]]]
+    
+    my_dict = [[{'00001' : 1, '24324' : 0},{'13' : 1, '24' : 0}],[{'13124' : 1, '24324' : 0},{'13' : 1, '24' : 0}]]
+    di = []
+    for j in range(len(my_dict)):
+        for i in range(len(my_dict[j])):
+            for key, value in my_dict[j][i].items():
+                temp = [key,value]
+                di.append(temp)
+    
+    di = np.asarray(di).astype(np.int32)
+    
+    
+    starttest = time.time()
+    addi = addition(di)
+    endtest = time.time()
+    print("time test = ",endtest - starttest)
+    print(addi)
+    
+#    print(type(d0))
+#    print(type(d))
+#    d = np.asarray(d).astype(np.int32)
+#    #print(type(d[0][0][0][0]))
+#    addi = addition(d)
+#    print(addi)
+
+
+
